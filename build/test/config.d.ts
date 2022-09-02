a22f00bef88bcc200742bb5afeb5e5493e2eb7b1
LexLuengas <alexis.luengas@gmail.com>
2022-09-02 16:58:47 +0200

    build: remove build from version control

--- build/test/config.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const MOCK_SERVER_DIR: string;
-export declare const SCHEMAS_DIR: string;
-export declare const PROXY_PORT = 8888;
-export declare const TARGET_SERVER_PORT = 8889;
-export declare const DEFAULT_OPENAPI_FILE: string;
--- build/test/util/process.d.ts
+++
@@ -1,6 +0,0 @@
-/** Kills all processes that are listening on a given port. */
-export declare function killOnPort(port: number | string): Promise<any>;
-/** Kills many processes by their PIDs. */
-export declare function killProcesses(pids: number[]): Promise<Array<any>>;
-/** Kills a process given its PID. */
-export declare function killProcess(pid: number): Promise<any>;
